4ed01ed19a5f4ec70cf3333b9a94ca456d005141
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-19 11:26:56 -0700

    fix typo

--- src/App.js
+++ src/App.js
@@ -1,139 +1,141 @@
-import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import CarNew from "./pages/CarNew";
-import CarShow from "./pages/CarShow";
-import CarEdit from "./pages/CarEdit";
-import MyCars from "./pages/MyCars";
-import NotFound from "./pages/NotFound";
-import AboutUs from "./pages/AboutUs";
-import CarIndex from "./pages/CarIndex";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import "./App.css";
-import mockCars from "./mockCars";
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import CarNew from './pages/CarNew'
+import CarShow from './pages/CarShow'
+import CarEdit from './pages/CarEdit'
+import MyCars from './pages/MyCars'
+import NotFound from './pages/NotFound'
+import AboutUs from './pages/AboutUs'
+import CarIndex from './pages/CarIndex'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import './App.css'
+import mockCars from './mockCars'
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = 'http://localhost:3000'
 
 const App = () => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null)
 
-  const [cars, setCars] = useState(mockCars);
+  const [cars, setCars] = useState(mockCars)
 
   const createCar = (newCar) => {
     fetch(`${BASE_URL}/cars`, {
       body: JSON.stringify(newCar),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      method: "POST",
+      method: 'POST',
     })
       .then((res) => res.json())
       .then(() => readCars())
-      .catch((error) => console.error("Car create error: ", error));
-  };
+      .catch((error) => console.error('Car create error: ', error))
+  }
 
   const readCars = () => {
     fetch(`${BASE_URL}/cars`)
       .then((res) => res.json())
       .then((data) => setCars(data))
-      .catch((error) => console.error("Car read error: ", error));
-  };
+      .catch((error) => console.error('Car read error: ', error))
+  }
 
   const updateCar = (newCar, id) => {
     fetch(`${BASE_URL}/cars/${id}`, {
       body: JSON.stringify(newCar),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      method: "PATCH",
+      method: 'PATCH',
     })
       .then((response) => response.json())
       .then(() => readCars())
-      .catch((error) => console.log("Updated car errors: ", error));
-  };
+      .catch((error) => console.log('Updated car errors: ', error))
+  }
 
   const deleteCar = (id) => {
     fetch(`${BASE_URL}/cars/${id}`, {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-      method: "DELETE",
+      method: 'DELETE',
     })
       .then((response) => response.json())
       .then(() => readCars())
-      .catch((error) => console.log("Car delete error", error, id));
-  };
+      .catch((error) => console.log('Car delete error', error, id))
+  }
 
   const register = (email, password) => {
     const userInfo = {
       user: { email, password },
-    };
-    body: JSON.stringify(userInfo);
-    headers: {
-      "Content-Type", "application/json";
-      Accept: "application/json";
     }
-    method: "POST",
-      fetch(`${BASE_URL}/register`)
-        .then((res) => {
-          localStorage.setItem("token", response.headers.get("Authorization"));
-          return res.json();
-        })
 
-        .then((data) => {
-          if (data?.error) {
-            console.error(data.error);
-          } else {
-            setCurrentUser(data);
-          }
-        });
-  };
+    fetch(`${BASE_URL}/register`, {
+      body: JSON.stringify(userInfo),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      method: 'POST',
+    })
+      .then((res) => {
+        localStorage.setItem('token', response.headers.get('Authorization'))
+        return res.json()
+      })
+      .then((data) => {
+        if (data?.error) {
+          console.error(data.error)
+        } else {
+          setCurrentUser(data)
+        }
+      })
+  }
 
   const login = (email, password) => {
     const userInfo = {
       user: { email, password },
-    };
-    body: JSON.stringify(userInfo);
-    headers: {
-      "Content-Type", "application/json";
-      Accept: "application/json";
     }
-    method: "POST",
-      fetch(`${BASE_URL}/login`)
-        .then((res) => {
-          localStorage.setItem("token", response.headers.get("Authorization"));
-          return res.json();
-        })
 
-        .then((data) => {
-          if (data?.error) {
-            console.error(data.error);
-          } else {
-            setCurrentUser(data);
-          }
-        });
-  };
+    fetch(`${BASE_URL}/login`, {
+      body: JSON.stringify(userInfo),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      method: 'POST',
+    })
+      .then((res) => {
+        localStorage.setItem('token', response.headers.get('Authorization'))
+        return res.json()
+      })
+      .then((data) => {
+        if (data?.error) {
+          console.error(data.error)
+        } else {
+          setCurrentUser(data)
+        }
+      })
+  }
 
   useEffect(() => {
-    readCars();
-  }, []);
+    readCars()
+  }, [])
 
   return (
     <>
       <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/carnew" element={<CarNew createCar={createCar} />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/carnew' element={<CarNew createCar={createCar} />} />
           <Route
-            path="/carshow/:id"
+            path='/carshow/:id'
             element={<CarShow cars={cars} current_user={currentUser} />}
           />
           <Route
-            path="/caredit/:id"
+            path='/caredit/:id'
             element={
               <CarEdit
                 cars={cars}
@@ -144,19 +146,19 @@ const App = () => {
             }
           />
           <Route
-            path="/mycars"
+            path='/mycars'
             element={<MyCars cars={cars} current_user={currentUser} />}
           />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/carindex" element={<CarIndex cars={cars} />} />
-          <Route path="/login" element={<Login login={login} />} />
-          <Route path="/register" element={<Register register={register} />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path='/aboutus' element={<AboutUs />} />
+          <Route path='/carindex' element={<CarIndex cars={cars} />} />
+          <Route path='/login' element={<Login login={login} />} />
+          <Route path='/register' element={<Register register={register} />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </main>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
