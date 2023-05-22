4e3d5fcc1730a4dc260a8c614faf9fd24f4d6cc1
LEARN Academy <sdunlap102326@gmail.com>
2023-05-22 14:58:49 -0700

    Added photos to about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,7 +4,7 @@ import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap'
 const AboutUs = () => {
   return (
     <>
-    <h1 className='font-header'>About The Team</h1>
+    <h1 className='font-header text-center'>About The Team</h1>
           <br />
       <div className='card-container' style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <div className='card-background'>
@@ -13,19 +13,19 @@ const AboutUs = () => {
             
             <CardBody>
               <CardTitle tag='h5'>Nikki Dunlap</CardTitle>
-              <CardText tag='h4'>Design Lead</CardText>
+              <CardText tag='h4'>Design Lead says</CardText>
               <CardText>
                 <small className='smaller-card-text'>
-                  Schpeel about Nikki
+                  "I feel like this could look better."
                 </small>
               </CardText>
             </CardBody>
             <CardImg
               alt='Card image cap'
               bottom
-              src='https://picsum.photos/900/180'
+              src='https://media.licdn.com/dms/image/D5603AQGNjDqs3AsRiw/profile-displayphoto-shrink_800_800/0/1681834083841?e=1690416000&v=beta&t=SOIzIkDb90q8fC6-ZmvOJZipZuXmV1ksS1Ymf9-nFFw'
               style={{
-                height: 180,
+                height: 310,
               }}
               width='100%'
             />
@@ -35,17 +35,17 @@ const AboutUs = () => {
           <Card className='custom-card2'>
             <CardBody>
               <CardTitle tag='h5'>Ray Kneipp</CardTitle>
-              <CardText tag='h4'>Tech Lead</CardText>
+              <CardText tag='h4'>Tech Lead says</CardText>
               <CardText>
-                <small className='smaller-card-text'>Schpeel about Ray</small>
+                <small className='smaller-card-text'>"Uhhhhh..."</small>
               </CardText>
             </CardBody>
             <CardImg
               alt='Card image cap'
               bottom
-              src='https://picsum.photos/900/180'
+              src='https://media.licdn.com/dms/image/D5603AQH7TY0MOllXNA/profile-displayphoto-shrink_800_800/0/1667320867883?e=1690416000&v=beta&t=_58-LRfq39y2BaibrmLQ2xDV_87_-9T6eVAc9fs6hW0'
               style={{
-                height: 180,
+                height: 310,
               }}
               width='100%'
             />
@@ -55,17 +55,17 @@ const AboutUs = () => {
           <Card className='custom-card2'>
             <CardBody>
               <CardTitle tag='h5'>Greg Ostertag</CardTitle>
-              <CardText tag='h4'>Project Manager</CardText>
+              <CardText tag='h4'>Project Manager says</CardText>
               <CardText>
-                <small className='smaller-card-text'>Schpeel about Greg</small>
+                <small className='smaller-card-text'>"Please hire me!"</small>
               </CardText>
             </CardBody>
             <CardImg
               alt='Card image cap'
               bottom
-              src='https://picsum.photos/900/180'
+              src='https://media.licdn.com/dms/image/D4E03AQH2lSy0BZcFUQ/profile-displayphoto-shrink_800_800/0/1681246348521?e=1690416000&v=beta&t=jQsIpmDslqaOr4qqnAQBv1U0ye3bCQaXASi6gBDYKn0'
               style={{
-                height: 180,
+                height: 310,
               }}
               width='100%'
             />
@@ -77,15 +77,15 @@ const AboutUs = () => {
               <CardTitle tag='h5'>Apolonio Ortiz</CardTitle>
               <CardText tag='h4'>Product Manager</CardText>
               <CardText>
-                <small className='smaller-card-text'>Schpeel about Apolonio</small>
+                <small className='smaller-card-text'>"Hello" *British Accent*</small>
               </CardText>
             </CardBody>
             <CardImg
               alt='Card image cap'
               bottom
-              src='https://picsum.photos/900/180'
+              src='https://media.licdn.com/dms/image/D5635AQHclT2Iibn1Sg/profile-framedphoto-shrink_800_800/0/1682375613448?e=1685397600&v=beta&t=zwnSUNha-ALu-U0c_xcMY8z4TcRzqgdV_ifcoFAuAqI'
               style={{
-                height: 180,
+                height: 310,
               }}
               width='100%'
             />
