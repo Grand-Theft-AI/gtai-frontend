832aea1f55bd662d0f0ff57d0e420910b1b7c49f
SunGodApollo <apple.ortiz@hotmail.com>
2023-05-22 14:52:10 -0700

    working logout link I think

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import Container from './Container'
 import GTAI from '../assets/GTAI.png'
 
-const Navbar = ({ current_user }) => {
+const Navbar = ( {current_user, logout} ) => {
   return (
     <nav className='bg-black/50 backdrop-blur'>
       <Container className='flex items-center'>
@@ -13,9 +13,14 @@ const Navbar = ({ current_user }) => {
           <span className='nav-link-text'>Create</span>
         </Link>
         {current_user && (
+          <>
           <Link to='/mycars' className='nav-link'>
             <span className='nav-link-text'>Dashboard</span>
           </Link>
+          <Link to= '/' className='nav-link'>
+             <span className= 'nav-link-text' onClick={logout}>LOG OUT</span>
+          </Link>
+          </>
         )}
         <Link to='/carindex' className='nav-link'>
           <span className='nav-link-text'>View Listings</span>
