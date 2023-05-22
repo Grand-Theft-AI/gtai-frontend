58d83db90c16ffe741b5ec07b1468611a6be05c0
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-22 15:02:27 -0700

    select component

---
+++ src/components/Select.js
@@ -0,0 +1,25 @@
+import { useId } from 'react'
+
+const Select = ({ label, options, ...rest }) => {
+  const id = useId()
+
+  return (
+    <div className='flex flex-col'>
+      <label htmlFor={id}>{label}</label>
+
+      <select
+        id={id}
+        {...rest}
+        className='pt-2 pb-1 px-2 w-full flex-1 block border-white border bg-transparent rounded-md'
+      >
+        {options.map(({ value, label }) => (
+          <option value={value} key={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default Select
