cf9f59240cad3f3cb118be795d8e7d27f0cc08e5
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-23 18:51:52 -0700

    move team data to constants and changed styles

---
+++ src/constants/team.js
@@ -0,0 +1,42 @@
+export const team = [
+  {
+    name: 'Nikki Dunlap',
+    title: 'Design Lead',
+    quote: 'I feel like this could look better.',
+    description:
+      "Nikki really enjoys working on front-end development. Her dream job in tech is to design VST's for Image-Line.",
+    image:
+      'https://media.licdn.com/dms/image/D5603AQGNjDqs3AsRiw/profile-displayphoto-shrink_800_800/0/1681834083841?e=1690416000&v=beta&t=SOIzIkDb90q8fC6-ZmvOJZipZuXmV1ksS1Ymf9-nFFw',
+    linkedin: 'https://www.linkedin.com/in/nikkidunlap/',
+  },
+  {
+    name: 'Ray Kneipp',
+    title: 'Tech Lead',
+    quote: 'Uhhhhh...',
+    description:
+      'Ray takes pleasure in working on  development. His dream job in tech is to bakadabakada.',
+    image:
+      'https://media.licdn.com/dms/image/D5603AQH7TY0MOllXNA/profile-displayphoto-shrink_800_800/0/1667320867883?e=1690416000&v=beta&t=_58-LRfq39y2BaibrmLQ2xDV_87_-9T6eVAc9fs6hW0',
+    linkedin: 'https://www.linkedin.com/in/raymondkneipp/',
+  },
+  {
+    name: 'Greg Ostertag',
+    title: 'Project Manager',
+    quote: 'Please hire me!',
+    description:
+      'Greg likes working on bakadabakada development. His dream job in tech is to bakadabakada.',
+    image:
+      'https://media.licdn.com/dms/image/D4E03AQH2lSy0BZcFUQ/profile-displayphoto-shrink_800_800/0/1681246348521?e=1690416000&v=beta&t=jQsIpmDslqaOr4qqnAQBv1U0ye3bCQaXASi6gBDYKn0',
+    linkedin: 'https://www.linkedin.com/in/greg-ostertag/',
+  },
+  {
+    name: 'Apolonio Ortiz',
+    title: 'Product Manager',
+    quote: 'Hello *British Accent*',
+    description:
+      'Apolonio enjoys working on bakadabakada development. His dream job in tech is to bakadabakada.',
+    image:
+      'https://media.licdn.com/dms/image/D5635AQHclT2Iibn1Sg/profile-framedphoto-shrink_800_800/0/1682375613448?e=1685397600&v=beta&t=zwnSUNha-ALu-U0c_xcMY8z4TcRzqgdV_ifcoFAuAqI',
+    linkedin: 'https://www.linkedin.com/in/apolonioortiz/',
+  },
+]
