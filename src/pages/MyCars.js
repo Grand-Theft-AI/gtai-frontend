a2725115dd4d97015c66cb0e79679708119e3fcc
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-24 09:36:21 -0700

    add key to my cars

--- src/pages/MyCars.js
+++ src/pages/MyCars.js
@@ -49,7 +49,7 @@ const MyCars = ({ cars, current_user }) => {
 
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         {myCars.map((car) => (
-          <CarCard {...car} />
+          <CarCard {...car} key={car.id} />
         ))}
       </div>
     </Container>
