d57c023f5b26a4493c276abb6643a63b899581e0
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-24 09:29:35 -0700

    fix navbar links when authed and export base url

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,17 +18,17 @@ const Navbar = ({ current_user, logout }) => {
     <nav className='bg-black/50 backdrop-blur'>
       <Container className='flex md:flex-row flex-col md:items-center justify-between'>
         <div className='flex items-center justify-between flex-1'>
-        <Link to='/' className='logo-link'>
-          <img src={GTAI} alt='Logo' className='w-20 h-20 object-contain' />
-        </Link>
+          <Link to='/' className='logo-link'>
+            <img src={GTAI} alt='Logo' className='w-20 h-20 object-contain' />
+          </Link>
 
-        <button
-          onClick={toggleNav}
-          className='bg-white/5 hover:bg-white/20 focus:bg-white/20 outline-none rounded-md h-[36px] w-[36px] flex items-center justify-center md:hidden'
-          ref={menuButton}
-        >
-          {showNav ? <Close /> : <Menu />}
-        </button>
+          <button
+            onClick={toggleNav}
+            className='bg-white/5 hover:bg-white/20 focus:bg-white/20 outline-none rounded-md h-[36px] w-[36px] flex items-center justify-center md:hidden'
+            ref={menuButton}
+          >
+            {showNav ? <Close /> : <Menu />}
+          </button>
         </div>
 
         <div className={`items-center md:flex ${showNav ? '' : 'hidden'}`}>
@@ -38,7 +38,10 @@ const Navbar = ({ current_user, logout }) => {
           <Link to='/carnew' className='nav-link'>
             <span className='nav-link-text'>Create</span>
           </Link>
-          {current_user && (
+          <Link to='/carindex' className='nav-link'>
+            <span className='nav-link-text'>Listings</span>
+          </Link>
+          {current_user ? (
             <>
               <Link to='/mycars' className='nav-link'>
                 <span className='nav-link-text'>Dashboard</span>
@@ -49,11 +52,7 @@ const Navbar = ({ current_user, logout }) => {
                 </span>
               </Link>
             </>
-          )}
-          <Link to='/carindex' className='nav-link'>
-            <span className='nav-link-text'>Listings</span>
-          </Link>
-          {!current_user && (
+          ) : (
             <>
               <Link to='/login' className='nav-link'>
                 <span className='nav-link-text'>Login</span>
