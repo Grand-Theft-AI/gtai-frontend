c963a2a1167a0dc81e85020bfe0395e54d6690c0
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-24 10:17:02 -0700

    require values for inputs before form submission

--- src/pages/CarEdit.js
+++ src/pages/CarEdit.js
@@ -53,17 +53,20 @@ const CarEdit = ({ updateCar, deleteCar, cars }) => {
             options={carMakes}
             value={make}
             onChange={(e) => setMake(e.target.value)}
+            required
           />
           <LabeledInput
             label='Model'
             value={model}
             onChange={(e) => setModel(e.target.value)}
+            required
           />
           <LabeledInput
             label='Year'
             type='number'
             value={year}
             onChange={(e) => setYear(e.target.value)}
+            required
           />
         </div>
         <LabeledInput
@@ -71,33 +74,39 @@ const CarEdit = ({ updateCar, deleteCar, cars }) => {
           type='number'
           value={mileage}
           onChange={(e) => setMileage(e.target.value)}
+          required
         />
         <LabeledInput
           label='Image'
           value={image}
           onChange={(e) => setImage(e.target.value)}
+          required
         />
         <LabeledInput
           label='Price'
           type='number'
           value={price}
           onChange={(e) => setPrice(e.target.value)}
+          required
         />
         <LabeledInput
           label='Description'
           value={description}
           onChange={(e) => setDescription(e.target.value)}
+          required
         />
         <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
           <LabeledInput
             label='Street'
             value={street}
             onChange={(e) => setStreet(e.target.value)}
+            required
           />
           <LabeledInput
             label='City'
             value={city}
             onChange={(e) => setCity(e.target.value)}
+            required
           />
 
           <Select
@@ -105,26 +114,28 @@ const CarEdit = ({ updateCar, deleteCar, cars }) => {
             options={stateLabelValues}
             value={state}
             onChange={(e) => setState(e.target.value)}
+            required
           />
 
           <LabeledInput
             label='Zip'
             value={zip}
             onChange={(e) => setZip(e.target.value)}
+            required
           />
         </div>
 
-          <div className='grid gap-2 sm:grid-cols-2'>
-        <Button
-          color='danger'
-          onClick={() => {
-            deleteCar(id)
-          }}
-          type='button'
-        >
-          Delete Car
-        </Button>
-        <Button type='submit'>Update Car</Button>
+        <div className='grid gap-2 sm:grid-cols-2'>
+          <Button
+            color='danger'
+            onClick={() => {
+              deleteCar(id)
+            }}
+            type='button'
+          >
+            Delete Car
+          </Button>
+          <Button type='submit'>Update Car</Button>
         </div>
       </form>
     </Container>
