056d962e395ff0bfc09be4e26528b033fccbac96
LEARN Academy <sdunlap102326@gmail.com>
2023-05-24 11:55:55 -0700

    completed bios on team page

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,5 +1,5 @@
 // prod
-export const baseUrl = 'https://gtai-backend.onrender.com'
+// export const baseUrl = 'https://gtai-backend.onrender.com'
 
 // dev
-// export const baseUrl = 'http://localhost:3000'
+export const baseUrl = 'http://localhost:3000'
--- src/constants/team.js
+++ src/constants/team.js
@@ -1,3 +1,6 @@
+import Apple from '../assets/Apple.png'
+
+
 export const team = [
   {
     name: 'Nikki Dunlap',
@@ -14,7 +17,7 @@ export const team = [
     title: 'Tech Lead',
     quote: 'Uhhhhh...',
     description:
-      'Ray takes pleasure in working on  development. His dream job in tech is to bakadabakada.',
+      'Ray loves full stack development with a focus on type safety. His dream job is working as a digital architect, creating innovative solutions that transform industries through seamless integration of technology and user experience.',
     image:
       'https://media.licdn.com/dms/image/D5603AQH7TY0MOllXNA/profile-displayphoto-shrink_800_800/0/1667320867883?e=1690416000&v=beta&t=_58-LRfq39y2BaibrmLQ2xDV_87_-9T6eVAc9fs6hW0',
     linkedin: 'https://www.linkedin.com/in/raymondkneipp/',
@@ -24,7 +27,7 @@ export const team = [
     title: 'Project Manager',
     quote: 'Please hire me!',
     description:
-      'Greg likes working on bakadabakada development. His dream job in tech is to bakadabakada.',
+      'Greg likes working on backend development, his dream job in tech is to work on military weapon systems.',
     image:
       'https://media.licdn.com/dms/image/D4E03AQH2lSy0BZcFUQ/profile-displayphoto-shrink_800_800/0/1681246348521?e=1690416000&v=beta&t=jQsIpmDslqaOr4qqnAQBv1U0ye3bCQaXASi6gBDYKn0',
     linkedin: 'https://www.linkedin.com/in/greg-ostertag/',
@@ -34,9 +37,9 @@ export const team = [
     title: 'Product Manager',
     quote: 'Hello *British Accent*',
     description:
-      'Apolonio enjoys working on bakadabakada development. His dream job in tech is to bakadabakada.',
+      'Apolonio enjoys working on development of different things such as websites. His dream job in tech is to be a developer for either Sony or PlayStation.',
     image:
-      'https://media.licdn.com/dms/image/D5635AQHclT2Iibn1Sg/profile-framedphoto-shrink_800_800/0/1682375613448?e=1685397600&v=beta&t=zwnSUNha-ALu-U0c_xcMY8z4TcRzqgdV_ifcoFAuAqI',
+      Apple,
     linkedin: 'https://www.linkedin.com/in/apolonioortiz/',
   },
 ]
