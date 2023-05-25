fb74335c641872f265e17ea36a02fcbfad00a077
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-25 11:54:18 -0700

    remove spacing

--- src/__tests__/CarShow.test.js
+++ src/__tests__/CarShow.test.js
@@ -1,25 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import CarShow from "../pages/CarShow";
-import { MemoryRouter, Routes, Route} from "react-router-dom";
-import mockCars  from "../mockCars";
-import {users} from "../mockUsers";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import mockCars from "../mockCars";
+import { users } from "../mockUsers";
 
 describe("<CarShow />", () => {
   it("renders without crashing", () => {
-    // console.log('mockCars', mockCars)
     render(
       <MemoryRouter initialEntries={["/carshow/1"]}>
         <Routes>
-          
-        
-            <Route path='/carshow/:id' element={<CarShow cars={mockCars} current_user={users[0]} />}/>
-
-          
-            </Routes>
+          <Route
+            path="/carshow/:id"
+            element={<CarShow cars={mockCars} current_user={users[0]} />}
+          />
+        </Routes>
       </MemoryRouter>,
     );
-    screen.logTestingPlaygroundURL()
-    const indexLink = screen.getByText(/ford/i, {exact:false});
+    screen.logTestingPlaygroundURL();
+    const indexLink = screen.getByText(/ford/i, { exact: false });
     expect(indexLink).toBeInTheDocument();
   });
 });
